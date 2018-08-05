a11bbabf26128461251536f9db88ee34404c1f6d
Kevin G. Fourie <kevin@4e.co.za>
2018-08-05 14:11:37 +0200

    IPFS update

--- ipfs.nix
+++ ipfs.nix
@@ -6,7 +6,11 @@
   ];
   services.ipfs = {
     enable = true;
+    swarmAddress = [ "/ip4/0.0.0.0/tcp/5000" "/ip6/::/tcp/5000" ];
+    gatewayAddress = "/ip4/0.0.0.0/tcp/8080";
+    autoMount = true;
     enableGC = true;
   };
+
 }
 
